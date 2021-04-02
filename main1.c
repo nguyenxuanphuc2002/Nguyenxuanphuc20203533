69ff22e163ae761403e8c39ef04d1bb21c25a7c9
nguyenxuanphuc2002 <81752065+nguyenxuanphuc2002@users.noreply.github.com>
2021-04-02 23:13:33 +0700

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	long int a;
+	long int b;
+	long int c;
+	long int d;
+	printf("so ban muon nhap la : ");
+	scanf("%d",&a,&b,&c,&d);
+	printf (" %d giay =%d gio  %d phut %d giay ",a,b=a/3600,c=a/60%60,d=a%60);
+	
+	return 0 ;
+
+}
