e5aebb90e60599b03a4a8c9f177b12b4077d7098
nguyenxuanphuc2002 <81752065+nguyenxuanphuc2002@users.noreply.github.com>
2021-04-04 01:30:52 +0700

    Add files via upload

--- BT2.buoi2.c
+++ BT2.buoi2.c
@@ -11,7 +11,7 @@ int main() {
 	scanf("%d",&a);
 	printf("Thoi gian tieu thu: ");
 	scanf("%d",&b);
-	 printf("cong suat su dung: %.4f kWh \n chi phi dien hang thang: %.4f dong",c=a/b/1000, d=a/b/1000*30*24*3500);
+	 printf("cong suat su dung: %.4f kWh \n chi phi dien hang thang: %.4f dong",c=a/b/1000, d=a/b/1000*30*24*gd);
 	
 	return 0;
 }
