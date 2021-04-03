84316e60cd22bf57390e017b5f19d7089fb3d9e3
nguyenxuanphuc2002 <81752065+nguyenxuanphuc2002@users.noreply.github.com>
2021-04-04 00:34:57 +0700

    Add files via upload

---
+++ BT2.buoi2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define gd 3500
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int a,b;
+	float c,d;
+	printf("Nhap vao don vi cong: ");
+	scanf("%d",&a);
+	printf("Thoi gian tieu thu: ");
+	scanf("%d",&b);
+	 printf("cong suat su dung: %.4f kWh \n chi phi dien hang thang: %.4f dong",c=a/b/1000, d=a/b/1000*30*24*3500);
+	
+	return 0;
+}
