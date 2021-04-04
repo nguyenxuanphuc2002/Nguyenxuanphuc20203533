a1135d70dcb1a2b4a47cdfa3833aa957f5601e17
nguyenxuanphuc2002 <81752065+nguyenxuanphuc2002@users.noreply.github.com>
2021-04-04 16:37:30 +0700

    Update BT2.buoi2.c

--- BT2.buoi2.c
+++ BT2.buoi2.c
@@ -5,13 +5,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int a,b;
-	float c,d;
+	float a,b,c,d;
 	printf("Nhap vao don vi cong: ");
-	scanf("%d",&a);
+	scanf("%f",&a);
 	printf("Thoi gian tieu thu: ");
-	scanf("%d",&b);
-	 printf("cong suat su dung: %.4f kWh \n chi phi dien hang thang: %.4f dong",c=a/b/1000, d=a/b/1000*30*24*gd);
+	scanf("%f",&b);
+	 printf("cong suat su dung: %.2f kWh \n chi phi dien hang thang: %.2f dong",c=a/b/1000, d=a/b/1000*30*24*gd);
 	
 	return 0;
 }
